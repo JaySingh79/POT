b2fa0aa50f41c56c2ae936d5e28bbd4a50a518bb
Rémi Flamary <remi.flamary@gmail.com>
2026-07-29 08:57:32 +0200

    change version and decsribe relese in release file

--- ot/__init__.py
+++ ot/__init__.py
@@ -94,7 +94,7 @@ from .bsp import compute_bspot_bijection, merge_bijections
 from .utils import dist, unif, tic, toc, toq
 
 
-__version__ = "0.9.7"
+__version__ = "0.9.7.post1"
 
 __all__ = [
     "emd",
